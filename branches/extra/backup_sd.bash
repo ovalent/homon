01627e6cb0efb16a2743ff54922c6d6487f3b3c6
ovalent@gmail.com <ovalent@gmail.com@28e46a77-2840-b2be-fd7c-d0075d25f225>
2015-01-03 14:07:05 +0000

    minor update on backup SD script: add steps info and format SD card size output

--- branches/extra/backup_sd.bash
+++ branches/extra/backup_sd.bash
@@ -17,17 +17,18 @@ VAR_outputfolder=/home/oli/raspberry/sd_images/
 
 #get SD card size
 VAR_SDsize=$(blockdev --getsize64 $VAR_SDcard)
-echo SD card: $VAR_SDcard \(size: $VAR_SDsize\)
+VAR_SDsize_f=$(numfmt --to=iec-i --suffix=B $VAR_SDsize)
+echo SD card: $VAR_SDcard \(size: $VAR_SDsize_f\)
 
-echo Creating img file from SD card \($VAR_outputfolder$VAR_outputfile\)...
+echo [1/3] Creating img file from SD card \($VAR_outputfolder$VAR_outputfile\)...
 #dd if=$VAR_SDcard of=$VAR_outputfolder$VAR_outputfile;
 pv -tpreb -s $VAR_SDsize $VAR_SDcard | dd of=$VAR_outputfolder$VAR_outputfile;
 #dd if=$VAR_SDcard bs=4096 | pv -tpreb -s $VAR_SDsize | dd bs=4096 of=$VAR_outputfolder$VAR_outputfile
 
-echo Compressing $VAR_outputfile to $VAR_outputzipfile...
+echo [2/3] Compressing $VAR_outputfile to $VAR_outputzipfile...
 pv -tpreb $VAR_outputfolder$VAR_outputfile | pigz > $VAR_outputfolder$VAR_outputzipfile
 
-echo Delete $VAR_outputfile file...
+echo [3/3] Delete $VAR_outputfile file...
 rm $VAR_outputfolder$VAR_outputfile
 
 echo Done !
