cdd860087b90b47488842ff08a4583e6e2d3306b
ovalent@gmail.com <ovalent@gmail.com@28e46a77-2840-b2be-fd7c-d0075d25f225>
2015-01-03 21:06:46 +0000

    add echo messages and mount folder variables

--- branches/extra/homon_mount.bash
+++ branches/extra/homon_mount.bash
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+VAR_mount_folder=/home/oli/raspberry/oli-pi
+
 #sshfs pi@192.168.0.44:/home/pi/homon /home/oli/homon_oli-pi/
 #sshfs -o idmap=user -o gid=`id --group` pi@192.168.0.44:/home/pi/ /home/oli/raspberry/oli-pi
-sshfs pi@192.168.0.44:/home/pi/ /home/oli/raspberry/oli-pi
+sshfs pi@192.168.0.44:/home/pi/ $VAR_mount_folder
+echo pi@oli-pi home mounted in $VAR_mount_folder.
 
