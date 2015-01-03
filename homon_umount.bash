25efcd5ab26b41c39b3fa71b19ad1a38f148aa5b
ovalent@gmail.com <ovalent@gmail.com@28e46a77-2840-b2be-fd7c-d0075d25f225>
2015-01-03 21:06:46 +0000

    add echo messages and mount folder variables

--- homon_umount.bash
+++ homon_umount.bash
@@ -1,3 +1,4 @@
 #!/bin/bash
-
-fusermount -u /home/oli/raspberry/oli-pi
+VAR_mount_folder=/home/oli/raspberry/oli-pi
+fusermount -u $VAR_mount_folder
+echo pi@oli-pi folder $VAR_mount_folder UNmounted. 
