2e5aff170f38d229d21bd4dd5305ed7b072cdb5d
ovalent@gmail.com <ovalent@gmail.com@28e46a77-2840-b2be-fd7c-d0075d25f225>
2015-01-31 21:41:10 +0000

    add cpu temperature. miscellaneous minor optimisations

--- branches/cpu_temperature.py
+++ branches/cpu_temperature.py
@@ -2,19 +2,29 @@
 # -*- coding: utf-8 -*-
 
 import subprocess
+import psutil
 
-class Sensor_temperature_cpu(object):  
+class RPI_Data(object):  
   
-  sensor_name = "CPU temperature"  
-  sensor_unit = "ºC"
-  sensor_desc = "onboard Raspberry Pi B+"
+    sensor_name = "CPU temperature"  
+    sensor_unit = "°C"
+    sensor_desc = "onboard Raspberry Pi B+"
   
-  def get_sensor_value(self):
+    def readTemperature(self):
       #"Returns the temperature in degrees celcius"
       try:
 	    s = subprocess.check_output(["/opt/vc/bin/vcgencmd","measure_temp"])
 	    return float(s.split('=')[1][:-3])
       except:
-	    return -99
+	    return -255
+	    
+    def readCPU(self):
+        # return CPU utilization in %
+        psutil.cpu_percent(interval=0.1)
+    
+    def readMemory(self):
+        # return (total - available) memory in %
+        return psutil.virtual_memory().percent
+  
 
 
--- branches/run2.py
+++ branches/run2.py
@@ -19,6 +19,9 @@ from optparse import OptionParser
 # import LCD class
 from lcd_display import LCD_display
 
+# import a class to get rapsberry pi values (CPU temp, CPU load, Mem)
+from cpu_temperature import RPI_Data
+
 # Now import what we need
 import time
 import datetime
@@ -33,6 +36,8 @@ import RT_NullSensor
 
 # the sensor update interval (in seconds). Change as required.
 SENSOR_UPDATE_INTERVAL = 5
+# data upload interval (in seconds). Change as required.
+DATA_UPLOAD_INTERVAL = 60
 
 # The set of sensors. Choose which ones are required or use NullSensor if no physical sensor
 # Multi sensor objects (such as BMP180 for temp and pressure) can be reused
@@ -42,6 +47,8 @@ s_temperature = RT_MCP9808.RT_MCP9808()
 s_pressure = RT_BMP180.RT_BMP180()
 s_humidity = RT_HTU21D.RT_HTU21D()
 
+# Raspberry Pi Data class (cpu temperature, CPU load, memory)
+s_rpi = RPI_Data()
 
 
 '''
@@ -75,26 +82,47 @@ def readSensors():
         s_humidity.background()
         return
         
-    # time send send the sensor readings    
+    # time send the sensor readings    
     lastSensorReadTime = time.time()
     
-   
-    print_verbose(datetime.datetime.now().isoformat())    
+    print_verbose("-------------------------------------------------")    
+    #print_verbose(datetime.datetime.now().isoformat()) 
+    print_verbose(datetime.datetime.fromtimestamp(lastSensorReadTime).strftime('%Y-%m-%d %H:%M:%S'))   
+    print_verbose("--------------------------") 
+    # light sensor TSL2561
     if s_light.getDataValid():
         lightData = s_light.readLight()
-        print_verbose("Light: %.2f lux" % lightData)
+        print_verbose("TSL2561 Light: %.2f lux" % lightData)
+    # temperature sensor MCP9808
     if s_temperature.getDataValid():
         temperatureData = s_temperature.readTemperature()
         #display_lcd.temperature(temperatureData)
-        print_verbose("Temperature: %.2f°C" % temperatureData)
+        print_verbose("MCP9808 Temperature: %.2f°C" % temperatureData)
+    # humidity sensor HTU21D    
     if s_humidity.getDataValid():
         humidityData = s_humidity.readHumidity()
+        humidityTemperatureData = s_humidity.readTemperature()
         #display_lcd.humidity(humidityData)
-        print_verbose("Humidity: %.2f%%RH" % humidityData)    
+        print_verbose("HTU21D Humidity: %.2f%%RH" % humidityData)  
+        print_verbose("HTU21D Temperature: %.2f°C" % humidityTemperatureData)    
+    # pressure sensor BMP180
     if s_pressure.getDataValid():
         pressureData = s_pressure.readPressure()
+        pressureTemperatureData = s_pressure.readTemperature()
         #display_lcd.pressure(pressureData)
-        print_verbose("Pressure: %.2fhPa" % pressureData)        
+        print_verbose("BMP180 Pressure: %.2fhPa" % pressureData)
+        print_verbose("BMP180 Temperature: %.2f°C" % pressureTemperatureData)    
+    # raspberry pi CPU temperature
+    rpiTempetatureData = s_rpi.readTemperature()
+    print_verbose("RPI Temperature: %.2f°C" % rpiTempetatureData)  
+    
+    # build string to display to LCD
+    lcd_string = " " + datetime.datetime.now().strftime(' %a %d %b - %H:%M') + "\n"
+    lcd_string = lcd_string + "Temperature: %.2f" % temperatureData + chr(223) + "C" + "\n" # chr(223) stand to replace degree "°" symbol
+    lcd_string = lcd_string + "Humidity:   %.2f%%RH" % humidityData + "\n"
+    lcd_string = lcd_string + "Pressure: %.2fhPa" % pressureData
+    # display it on LCD  
+    display_lcd.lcd.message(lcd_string)
     
 
 '''
@@ -114,11 +142,10 @@ def print_verbose(msg):
 
 def mLoop():
     ''' This is the main loop. '''
-
-    while True:
-        # update the date on lcd
-        display_lcd.date()
-        
+    
+    print_verbose("Initializing...")
+    
+    while True:        
         # see if anything from the sensors
         readSensors()
             
