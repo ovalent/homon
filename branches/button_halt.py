bfb40e6ecbbd612e77415a9e65a50c606d0a2707
ovalent@gmail.com <ovalent@gmail.com@28e46a77-2840-b2be-fd7c-d0075d25f225>
2015-02-03 19:13:47 +0000

    added WAIT_FOR 5 seconds pressed button before to shutdown

--- branches/button_halt.py
+++ branches/button_halt.py
@@ -11,16 +11,31 @@ import os
 import time
 from lcd_display import LCD_display
 
-GPIO.setmode(GPIO.BOARD)
 # GPIO21 set up as input on pin 40. It is pulled up to stop false signals 
-GPIO_chanel = 40
+GPIO_CHANEL = 40
+
+# set the time (in seconds) the button has to be pressed before shutdown runs
+WAIT_FOR = 5 
+
+# set the mode with pin number (1 to 40)
+GPIO.setmode(GPIO.BOARD)
 
 # It is pulled up to stop false signals 
-GPIO.setup(GPIO_chanel, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(GPIO_CHANEL, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 try:
-    #wait for event (press switch)
-    GPIO.wait_for_edge(GPIO_chanel, GPIO.FALLING)
+    buttonReleased = True
+    while buttonReleased:
+        #wait for event (press switch)
+        GPIO.wait_for_edge(GPIO_CHANEL, GPIO.FALLING)
+        # button has been pressed
+        buttonReleased = False
+        # wait loop set with WAIT_FOR seconds         
+        for i in range(WAIT_FOR * 10):
+            time.sleep(0.1)
+            if GPIO.input(GPIO_CHANEL):
+                buttonReleased = True
+                break
 
     # stop run2.py process
     os.system("pkill -f run2.py")
@@ -36,6 +51,6 @@ try:
     os.system("sudo shutdown -h now \"Halted by pressing button.\"")
     
 except KeyboardInterrupt:  
-  GPIO.cleanup(GPIO_chanel) # clean up GPIO on CTRL+C exit 
+  GPIO.cleanup(GPIO_CHANEL) # clean up GPIO on CTRL+C exit 
   
-GPIO.cleanup(GPIO_chanel)   # clean up GPIO on normal exit  
+GPIO.cleanup(GPIO_CHANEL)   # clean up GPIO on normal exit  
